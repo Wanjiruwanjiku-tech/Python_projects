27d740a1023a3ab1f5d13fe80f30660c0d5f1a19
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-07-04 05:37:20 +0300

    Tuples

---
+++ DataStructures/Tuples/my_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+my_tuple = (1, 2, 3, 4, 5, 6, 7, 8)
+print(my_tuple, "\n")
+print("Hi I am  Tuple, a data structure in Python\n")
+print("Pepole say I am a collection of ordered items of any data type but Immutable\n")
+print("This Means that the items I contain, can not be changed but they can be accessed\n")
+
+print("Me as whole: {} ".format( my_tuple))
+print("items: ")
+print(my_tuple[0])
+print(my_tuple[1])
+print(my_tuple[2])
+print(my_tuple[3])
+print(my_tuple[4])
+print(my_tuple[5])
+print(my_tuple[6])
+print(my_tuple[7])
